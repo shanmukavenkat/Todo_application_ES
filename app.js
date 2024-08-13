c70e3d34f5ca1ca144e7eef3a1a9c8ad696a16e3
A.S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-08-13 07:31:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,176 +1,181 @@
+/*
+ *  Created a Table with name todo in the todoApplication.db file using the CLI.
+ */
+
 const express = require('express')
-const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
+const path = require('path')
+
+const databasePath = path.join(__dirname, 'todoApplication.db')
 
 const app = express()
+
 app.use(express.json())
 
-const dbPath = path.join(__dirname, 'todoApplication.db')
+let database = null
 
-let db = null
-
-const initializeDbServer = async () => {
+const initializeDbAndServer = async () => {
   try {
-    db = await open({
-      filename: dbPath,
+    database = await open({
+      filename: databasePath,
       driver: sqlite3.Database,
     })
-    app.listen(3000, () => {
-      console.log('The server is running on http://localhost:3000')
-    })
-  } catch (e) {
-    console.log(e.message)
+
+    app.listen(3000, () =>
+      console.log('Server Running at http://localhost:3000/'),
+    )
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`)
     process.exit(1)
   }
 }
 
-initializeDbServer()
+initializeDbAndServer()
 
-const hasPriorityAndStatus = requestQuery => {
+const hasPriorityAndStatusProperties = requestQuery => {
   return (
     requestQuery.priority !== undefined && requestQuery.status !== undefined
   )
 }
 
-const hasPriority = requestQuery => {
+const hasPriorityProperty = requestQuery => {
   return requestQuery.priority !== undefined
 }
 
-const hasStatus = requestQuery => {
+const hasStatusProperty = requestQuery => {
   return requestQuery.status !== undefined
 }
-
 app.get('/todos/', async (request, response) => {
-  let getTodoQuery = ''
+  let data = null
+  let getTodosQuery = ''
   const {search_q = '', priority, status} = request.query
 
   switch (true) {
-    case hasPriorityAndStatus(request.query):
-      getTodoQuery = `
-        SELECT 
-        * 
-        FROM 
-        todo
-        WHERE 
+    case hasPriorityAndStatusProperties(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
         todo LIKE '%${search_q}%'
         AND status = '${status}'
         AND priority = '${priority}';`
       break
-    case hasPriority(request.query):
-      getTodoQuery = `
-        SELECT 
-        * 
-        FROM 
-        todo
-        WHERE 
+    case hasPriorityProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
         todo LIKE '%${search_q}%'
         AND priority = '${priority}';`
       break
-    case hasStatus(request.query):
-      getTodoQuery = `
-        SELECT 
-        * 
-        FROM 
-        todo
-        WHERE 
+    case hasStatusProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
         todo LIKE '%${search_q}%'
         AND status = '${status}';`
       break
     default:
-      getTodoQuery = `
-        SELECT 
-        * 
-        FROM 
-        todo
-        WHERE 
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
         todo LIKE '%${search_q}%';`
   }
 
-  console.log('Executing query:', getTodoQuery) // Debugging
-  try {
-    const data = await db.all(getTodoQuery)
-    response.send(data)
-  } catch (e) {
-    console.error('Error executing query:', e.message)
-    response.status(500).send({error: 'Internal Server Error'})
-  }
+  data = await database.all(getTodosQuery)
+  response.send(data)
 })
 
-app.get('/todos/:todoId', async (request, response) => {
+app.get('/todos/:todoId/', async (request, response) => {
   const {todoId} = request.params
-  const getTodoQuery = `SELECT * FROM todo WHERE id = ${todoId};`
 
-  console.log('Executing query:', getTodoQuery) // Debugging
-  try {
-    const todo = await db.get(getTodoQuery)
-    response.send(todo)
-  } catch (e) {
-    console.error('Error executing query:', e.message)
-    response.status(500).send({error: 'Internal Server Error'})
-  }
+  const getTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE
+      id = ${todoId};`
+  const todo = await database.get(getTodoQuery)
+  response.send(todo)
 })
 
 app.post('/todos/', async (request, response) => {
   const {id, todo, priority, status} = request.body
-  const postTodoQuery = `INSERT INTO todo (id, todo, priority, status) VALUES ('${id}', '${todo}', '${priority}', '${status}');`
-
-  console.log('Executing query:', postTodoQuery) // Debugging
-  try {
-    await db.run(postTodoQuery)
-    response.send('Todo Successfully Added')
-  } catch (e) {
-    console.error('Error executing query:', e.message)
-    response.status(500).send({error: 'Internal Server Error'})
-  }
+  const postTodoQuery = `
+  INSERT INTO
+    todo (id, todo, priority, status)
+  VALUES
+    (${id}, '${todo}', '${priority}', '${status}');`
+  await database.run(postTodoQuery)
+  response.send('Todo Successfully Added')
 })
 
 app.put('/todos/:todoId/', async (request, response) => {
   const {todoId} = request.params
   let updateColumn = ''
   const requestBody = request.body
-
-  const previousTodoQuery = `SELECT * FROM todo WHERE id = ${todoId};`
-
-  try {
-    const previousTodo = await db.get(previousTodoQuery)
-
-    const {
-      todo = previousTodo.todo,
-      priority = previousTodo.priority,
-      status = previousTodo.status,
-    } = request.body
-
-    const updateTodoQuery = `UPDATE todo SET todo = '${todo}', priority = '${priority}', status = '${status}' WHERE id = ${todoId};`
-
-    console.log('Executing query:', updateTodoQuery) // Debugging
-    await db.run(updateTodoQuery)
-
-    if (requestBody.status !== undefined) {
-      response.send('Status Updated')
-    } else if (requestBody.priority !== undefined) {
-      response.send('Priority Updated')
-    } else if (requestBody.todo !== undefined) {
-      response.send('Todo Updated')
-    }
-  } catch (e) {
-    console.error('Error executing query:', e.message)
-    response.status(500).send({error: 'Internal Server Error'})
+  switch (true) {
+    case requestBody.status !== undefined:
+      updateColumn = 'Status'
+      break
+    case requestBody.priority !== undefined:
+      updateColumn = 'Priority'
+      break
+    case requestBody.todo !== undefined:
+      updateColumn = 'Todo'
+      break
   }
+  const previousTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE 
+      id = ${todoId};`
+  const previousTodo = await database.get(previousTodoQuery)
+
+  const {
+    todo = previousTodo.todo,
+    priority = previousTodo.priority,
+    status = previousTodo.status,
+  } = request.body
+
+  const updateTodoQuery = `
+    UPDATE
+      todo
+    SET
+      todo='${todo}',
+      priority='${priority}',
+      status='${status}'
+    WHERE
+      id = ${todoId};`
+
+  await database.run(updateTodoQuery)
+  response.send(`${updateColumn} Updated`)
 })
 
 app.delete('/todos/:todoId/', async (request, response) => {
   const {todoId} = request.params
-  const deleteTodoQuery = `DELETE FROM todo WHERE id = ${todoId};`
+  const deleteTodoQuery = `
+  DELETE FROM
+    todo
+  WHERE
+    id = ${todoId};`
 
-  console.log('Executing query:', deleteTodoQuery) // Debugging
-  try {
-    await db.run(deleteTodoQuery)
-    response.send('Todo Deleted')
-  } catch (e) {
-    console.error('Error executing query:', e.message)
-    response.status(500).send({error: 'Internal Server Error'})
-  }
+  await database.run(deleteTodoQuery)
+  response.send('Todo Deleted')
 })
 
 module.exports = app
